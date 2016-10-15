2ed47b7f8e5e649867cfc81cbc67811e9c73eef2
saukymo <shaobo@mkdef.com>
2016-10-15 22:12:08 +0800

    add pages

--- demohour.py
+++ demohour.py
@@ -5,34 +5,45 @@ from StringIO import StringIO
 from lxml import etree
 from lxml.html.soupparser import fromstring
 
-
 def get_followers(user_id):
     url = "http://www.demohour.com/users/%s/followers" % user_id
-    return get_user_list(url)
+    xpath = "//html/body/div[2]/div[1]/div[2]/div[1]/a[2]/strong"
+    return get_user_list(url, xpath)
 
 def get_followed(user_id):
     url = "http://www.demohour.com/users/%s/followed" % user_id
-    return get_user_list(url)
+    xpath = "/html/body/div[2]/div[1]/div[2]/div[1]/a[1]/strong"
+    return get_user_list(url, xpath)
 
-def get_user_list(url):
+def get_user_list(url, xpath):
     # fetch html file and decode with 'utf-8'
     content = urllib2.urlopen(url).read().decode('utf-8')
     
     # parse html file into a tree and get user table
     root = fromstring(content)
     table = root.xpath('//html/body/div[2]/div[1]/div[1]/div[1]/dl')
-    
+    total = int(root.xpath(xpath)[0].text_content())
+
     user_list = []
-    for ele in table:
-        username = ele.xpath('dt/a[1]')[0].text_content().strip()
-        place = ele.xpath('dd[2]/p')[0].text_content().strip()
-        time = ele.xpath('dd[2]/p')[0].tail.split(' ')[0].strip()
-        data = ele.xpath('dd')[2].text_content().strip().split(' ')
-        news = int(data[1])
-        followed = int(data[3])
-        followers = int(data[5])
-        print username, place, time, news, followed, followers
-        user_list.append([username, place, time, news, followed, followers])
+    step = 1
+    while True:
+        step += 1
+        for ele in table:
+            username = ele.xpath('dt/a[1]')[0].text_content().strip()
+            place = ele.xpath('dd[2]/p')[0].text_content().strip()
+            time = ele.xpath('dd[2]/p')[0].tail.split(' ')[0].strip()
+            data = ele.xpath('dd')[2].text_content().strip().split(' ')
+            news = int(data[1])
+            followed = int(data[3])
+            followers = int(data[5])
+            print username, place, time, news, followed, followers
+            user_list.append([username, place, time, news, followed, followers])
+        if len(user_list) < total:
+            content = urllib2.urlopen(url + "?page=%d" % step).read().decode('utf-8')
+            root = fromstring(content)
+            table = root.xpath('//html/body/div[2]/div[1]/div[1]/div[1]/dl')
+        else:
+            break
     return user_list
 
 
